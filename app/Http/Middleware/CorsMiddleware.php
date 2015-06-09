73d1e46d2460fd4efced1921617754a8a89ad0c4
hush2 <hushywushy@gmail.com>
2015-06-09 16:21:46 +0800

    added other cors headers

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -2,7 +2,9 @@
 
 use Closure;
 
-class CorsMiddleware {
+class CorsMiddleware
+{
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
 
     public function handle($request, Closure $next)
     {
@@ -11,7 +13,16 @@ class CorsMiddleware {
         $response->header('Access-Control-Allow-Origin', $request->header('Origin', '*'));
         $response->header('Access-Control-Allow-Credentials', 'true');
 
+        if ($request->has('Origin')) {
+            $response->header('Vary', 'Origin');
+        }
+        if ($request->getMethod() == 'OPTIONS') {
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+            $response->header('Access-Control-Allow-Methods', '3600'); // 1 hr
+            if ($request->has('Access-Control-Request-Headers')) {
+                $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+            }
+        }
         return $response;
     }
-
 }
